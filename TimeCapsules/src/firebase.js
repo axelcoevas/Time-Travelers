64d90bdae7c883002827192596bee01ee0f8ff46
Axel Cuevas <axelcoevas@gmail.com>
2021-06-11 19:16:18 -0500

    Agregando archivo firebase

---
+++ TimeCapsules/src/firebase.js
@@ -0,0 +1,17 @@
+import firebase from 'firebase';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCIwJz8gplyzYYnMmKCj4NK2e6CD4K86dA",
+  authDomain: "time-travelers-d2c0e.firebaseapp.com",
+  projectId: "time-travelers-d2c0e",
+  storageBucket: "time-travelers-d2c0e.appspot.com",
+  messagingSenderId: "844546482097",
+  appId: "1:844546482097:web:915aa82aabcf28d208a790"
+};
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+
+export { db, auth };
