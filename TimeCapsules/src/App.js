7609527154b44a6b41cd085119e20eff3e8a9c5b
Axel Cuevas <axelcoevas@gmail.com>
2021-06-13 18:05:34 -0500

    Agregando 2do Header

--- TimeCapsules/src/App.js
+++ TimeCapsules/src/App.js
@@ -11,6 +11,8 @@ import RedSocialPerfil from './components/RedSocialPerfil';
 import RedSocialWishList from './components/RedSocialWishList';
 import RedSocial from './components/RedSocial';
 import MisCapsulas from './components/MisCapsulas';
+import HeaderSocialMedia from './components/HeaderSocialMedia';
+import CrearMiniCapsula from './components/CrearMiniCapsula';
 
 function App() {
   // eslint-disable-next-line
@@ -38,33 +40,57 @@ function App() {
   return (
     <Router>
       <div className="App">
-        <Header />
         <Switch>
-        <Route path="/MisCapsulas">
-          <MisCapsulas />
-        </Route>
-        <Route path="/RedSocial">
-          <RedSocial />
-        </Route>
-        <Route path="/RedSocialWishList">
+          <Route path="/RedSocial">
+            <HeaderSocialMedia />
             <RedSocial />
           </Route>
-          <Route path="/RedSocial">
-            <RedSocial />
+          <Route path="/CrearMiniCapsula">
+            <HeaderSocialMedia />
+            <CrearMiniCapsula />
           </Route>
           <Route path="/RedSocialWishList">
+            <HeaderSocialMedia />
             <RedSocialWishList />
           </Route>
           <Route path="/RedSocialPerfil">
+            <HeaderSocialMedia />
+            <RedSocialPerfil />
+          </Route>
+          <Route path="/MisCapsulas">
+            <Header />
+            <MisCapsulas />
+          </Route>
+          <Route path="/RedSocial">
+            <Header />
+            <RedSocial />
+          </Route>
+          <Route path="/RedSocialWishList">
+            <Header />
+            <RedSocial />
+          </Route>
+          <Route path="/RedSocial">
+            <Header />
+            <RedSocial />
+          </Route>
+          <Route path="/RedSocialWishList">
+            <Header />
+            <RedSocialWishList />
+          </Route>
+          <Route path="/RedSocialPerfil">
+            <Header />
             <RedSocialPerfil />
           </Route>
           <Route path="/CrearCapsula">
+            <Header />
             <CrearCapsula />
           </Route>
           <Route path="/Login">
+            <Header />
             <Login />
           </Route>
           <Route path="/">
+            <Header />
             <Home />
           </Route>
         </Switch>
--- TimeCapsules/src/components/MisCapsulas.js
+++ TimeCapsules/src/components/MisCapsulas.js
@@ -4,49 +4,49 @@ import capsule1 from '../resources/vr.jpg'
 import { Link } from 'react-router-dom'
 
 function MisCapsulas() {
-    return (
-        <div className="MisCapsulas">
-        <div className="cont-all-misCapsulas">
+  return (
+    <div className="MisCapsulas">
+      <div className="cont-all-misCapsulas">
         <div className="tit-misCapsulas">
-            <h3>Mis Capsulas</h3>
+          <h3>Mis Capsulas</h3>
         </div>
         <div className="cont-misCapsulas">
-            <div className="container-misCapsulas">
-                <div className="item-misCapsulas">
-                    <a href="" className="etiquetaA">
-                        <img src={capsule1} alt="" className="item-img"/>
-                        <div className="item-text">
-                            <h3>Capsula 1</h3>
-                            <p>Lorem ipsum dolor sit amet, consectetur.</p>
-                        </div>
-                        </a>
+          <div className="container-misCapsulas">
+            <div className="item-misCapsulas">
+              <span className="etiquetaA">
+                <img src={capsule1} alt="" className="item-img" />
+                <div className="item-text">
+                  <h3>Capsula 1</h3>
+                  <p>Lorem ipsum dolor sit amet, consectetur.</p>
                 </div>
-                <div className="item-misCapsulas">
-                    <a href="" className="etiquetaA">
-                        <img src={capsule1} alt="" className="item-img"/>
-                        <div className="item-text">
-                            <h3>Capsula 2</h3>
-                            <p>Lorem ipsum dolor sit amet, consectetur.</p>
-                        </div>
-                        </a>
-                </div>
-                <div className="item-misCapsulas">
-                    <a className="etiquetaA">
-                        <img src={capsule1} alt="" className="item-img"/>
-                        <div className="item-text">
-                            <h3>Capsula 3</h3>
-                            <p>Lorem ipsum dolor sit amet, consectetur.</p>
-                        </div>
-                    </a>
-                </div>
+              </span>
             </div>
-            <div className="button-misCapsulas">
-               <Link to="./CrearCapsula"><button>Nueva Capsula</button></Link>
+            <div className="item-misCapsulas">
+              <span className="etiquetaA">
+                <img src={capsule1} alt="" className="item-img" />
+                <div className="item-text">
+                  <h3>Capsula 2</h3>
+                  <p>Lorem ipsum dolor sit amet, consectetur.</p>
+                </div>
+              </span>
             </div>
+            <div className="item-misCapsulas">
+              <span className="etiquetaA">
+                <img src={capsule1} alt="" className="item-img" />
+                <div className="item-text">
+                  <h3>Capsula 3</h3>
+                  <p>Lorem ipsum dolor sit amet, consectetur.</p>
+                </div>
+              </span>
+            </div>
+          </div>
+          <div className="button-misCapsulas">
+            <Link to="./CrearCapsula"><button>Nueva Capsula</button></Link>
+          </div>
         </div>
+      </div>
     </div>
-    </div>
-    );
+  );
 }
-    
+
 export default MisCapsulas;
