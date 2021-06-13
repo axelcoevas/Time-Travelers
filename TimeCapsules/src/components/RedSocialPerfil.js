ccf6c533216ac59da0a4b27512714cb4ab78c0ca
Axel Cuevas <axelcoevas@gmail.com>
2021-06-13 16:42:37 -0500

    Fixing warnings

--- TimeCapsules/src/components/RedSocialPerfil.js
+++ TimeCapsules/src/components/RedSocialPerfil.js
@@ -9,51 +9,51 @@ import perfil from '../resources/perfil.jpg'
 function RedSocialPerfil() {
   return (
     <div className="Perfil">
-        <div className="cont-all">
+      <div className="cont-all">
         <div className="usuario-perfil">
-            <div className="img-usuario">
-                <img src={perfil} alt="Usuario" width="230" height="230"/>
-            </div>
-            <div className="datos-usuario">
-                <p>Nombre: </p>
-                <label>Gabriel</label>
-                <p>Usuario: </p>
-                <label>Gabo001</label>
-                <p>Correo:</p>
-                <label>jarzatel1400@alumno.ipn.mx</label>
-                <p>Fecha de Nacimiento: </p>
-                <label>16 - Julio - 1999</label>
-            </div>
+          <div className="img-usuario">
+            <img src={perfil} alt="Usuario" width="230" height="230" />
+          </div>
+          <div className="datos-usuario">
+            <p>Nombre: </p>
+            <label>Gabriel</label>
+            <p>Usuario: </p>
+            <label>Gabo001</label>
+            <p>Correo:</p>
+            <label>jarzatel1400@alumno.ipn.mx</label>
+            <p>Fecha de Nacimiento: </p>
+            <label>16 - Julio - 1999</label>
+          </div>
         </div>
         <div className="publicaciones-perfil">
-            <div className="title-wish">
-                <p>Mi WishList</p>
+          <div className="title-wish">
+            <p>Mi WishList</p>
+          </div>
+          <hr className="linea2" />
+          <div className="wish">
+            <div className="galeria">
+              <div className="foto">
+                <img src={amazon} alt="Amazon"></img>
+              </div>
             </div>
-            <hr className="linea2"/>
-            <div className="wish">
-                <div className="galeria">
-                    <div className="foto">
-                        <img src={amazon} alt="Amazon"></img>
-                    </div>
-                </div>
-                <div className="galeria">
-                    <div className="foto">
-                        <img src={mercadolibre} alt="MercadoLibre"></img>
-                    </div>
-                </div>
-                <div className="galeria">
-                    <div className="foto">
-                        <img src={claro}></img>
-                    </div>
-                </div>
-                <div className="galeria">
-                    <div className="foto">
-                        <img src={nike}></img>
-                    </div>
-                </div>
+            <div className="galeria">
+              <div className="foto">
+                <img src={mercadolibre} alt="MercadoLibre"></img>
+              </div>
             </div>
-         </div>
+            <div className="galeria">
+              <div className="foto">
+                <img src={claro} alt="claro"></img>
+              </div>
+            </div>
+            <div className="galeria">
+              <div className="foto">
+                <img src={nike} alt="nike"></img>
+              </div>
+            </div>
+          </div>
         </div>
+      </div>
     </div>
   );
 }
